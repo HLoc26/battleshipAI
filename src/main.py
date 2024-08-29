6de0c16519c3c1113312a4f364fde24ed0b5c80a
HLoc26 <dhl26052004@gmail.com>
2024-08-29 19:29:17 +0700

    testing scripts

--- src/main.py
+++ src/main.py
@@ -5,3 +5,16 @@ Created on Thu Aug 29 14:38:44 2024
 @author: ACER
 """
 
+from grid import Grid
+from ship import Ship
+
+GRID_SIZE = 10
+
+g = Grid(GRID_SIZE)
+
+s1 = Ship(3, True)
+s2 = Ship(5, False)
+
+g.add_ship(s2, (0, 9))
+
+print(g.grid)
