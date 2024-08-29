43875a8328679f8f859fc226acc56825dd0796b0
HLoc26 <dhl26052004@gmail.com>
2024-08-29 19:29:02 +0700

    update __init__()

--- src/ship.py
+++ src/ship.py
@@ -4,13 +4,11 @@ Created on Thu Aug 29 14:17:11 2024
 
 @author: ACER
 """
+from typing import List, Tuple
+
 
 class Ship():
-    def __init__(self, size: int) -> None:
+    def __init__(self, size: int, rotated: bool) -> None:
         self.size: int = size
-        self.rotate: bool = False # True = vertical, False = horizontal
-        self.pos: list[tuple[int, int]] = []
-        
-
-s = ship(3)
-print(s.pos)
+        self.isRotated: bool = rotated # True = vertical, False = horizontal
+        self.pos: List[Tuple[int, int]] = []
