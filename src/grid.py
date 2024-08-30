2dea0f54906b309c9227dcc1675a7af04b4ee397
HLoc26 <dhl26052004@gmail.com>
2024-08-30 09:20:16 +0700

    Update add_ship() for random ship placement

--- src/grid.py
+++ src/grid.py
@@ -17,53 +17,47 @@ class Grid():
         self.shipPos: Set[Tuple[int, int]] = set()
         self.shotSquares: Set[Tuple[int, int]] = set()
 
-    def add_ship(self, ship: Ship, pos: Tuple[int, int]) -> None:
+    def add_ship(self, ship: Ship, pos: Tuple[int, int]) -> bool:
         '''
         Add ship to the grid.
     
         :param ship: The Ship object.
         :param pos: the position where the user clicks to place the ship.
-        :returns: None.
+        :returns: True if placed successfully, False otherwise.
         :raises IndexError: When the ship is placed out of the grid, raise error.
         '''
-        def place_ship_segment(xPos, yPos) -> None:
-            '''Helper function to save the ship positions of squares
-            
-            :param xPos: The x position of square.
-            :param yPos: The y position of square.
-            :returns: None.
-            '''
-            curPos = (xPos, yPos)
-            ship.pos.append(curPos)
-            self.grid[xPos][yPos] = 1
-            self.shipPos.add(curPos)
-    
-        if ship.isRotated:  # Vertical ship
-            for i in range(ship.size):
-                try:
-                    xPos = pos[0] + i
-                    yPos = pos[1]
-                    if xPos >= self.GRID_SIZE:
-                        raise IndexError(f"Out of bound, moved from {pos} to.")
-                    place_ship_segment(xPos, yPos)
-                except IndexError as e:
-                    xPos = self.GRID_SIZE - 1 - i
-                    print(e, (xPos, yPos))
-                    place_ship_segment(xPos, yPos)
-    
-        else:  # Horizontal ship
-            for i in range(ship.size):
-                try:
-                    xPos = pos[0]
-                    yPos = pos[1] + i
-                    if yPos >= self.GRID_SIZE:
-                        raise IndexError(f"Out of bound, moved from {pos} to.")
-                    place_ship_segment(xPos, yPos)
-                except IndexError as e:
-                    yPos = self.GRID_SIZE - 1 - i
-                    print(e, (xPos, yPos))
-                    place_ship_segment(xPos, yPos)
+        def check_placeable(xPos: int, yPos: int) -> bool:
+            '''Check if the position is empty to place.'''
+            curPos: Tuple[int, int] = (xPos, yPos)
+            if curPos not in self.shipPos:
+                return True
+            return False
 
+        def get_positions(ship: Ship, pos: Tuple[int, int]) -> List[Tuple[int, int]]:
+            '''Returns squares which the ship is placed on.'''
+            positions: List[Tuple[int, int]] = []
+            for i in range(ship.size):
+                xPos: int = pos[0] + i if ship.isRotated else pos[0]
+                yPos: int = pos[1] if ship.isRotated else pos[1] + i
+                if xPos >= self.GRID_SIZE:
+                    xPos = self.GRID_SIZE - 1 - i
+                elif yPos >= self.GRID_SIZE:
+                    yPos = self.GRID_SIZE - 1 - i
+                canBePlaced: bool = check_placeable(xPos, yPos)
+                if canBePlaced: 
+                    positions.append((xPos, yPos))
+                else:
+                    return []
+            return positions
+        
+        curShipPos: List[Tuple[int, int]] = get_positions(ship, pos)
+        if len(curShipPos) == 0:
+            return False
+        for pos in curShipPos:
+            ship.pos.append(pos)
+            self.grid[pos[0]][pos[1]] = 1
+            self.shipPos.add(pos)
+        return True
 
     def shoot(self, attackPos: Tuple[int, int]) -> int:
         '''Return True if hit, else return False
