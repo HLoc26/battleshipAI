ea00d544e071448498255225227a05ea9c29a997
HLoc26 <dhl26052004@gmail.com>
2024-11-06 19:00:07 +0700

    update Direction

--- src/ship.py
+++ src/ship.py
@@ -2,10 +2,8 @@ from typing import List, Tuple
 from enum import Enum
 
 class Direction(Enum):
-    NORTH = (-1, 0)
-    SOUTH = (1, 0)
-    EAST = (0, 1)
-    WEST = (0, -1)
+    HORIZONTAL = (0, 1)
+    VERTICAL = (1, 0)
 
 class Ship:
     def __init__(self, name: str, length: int):
